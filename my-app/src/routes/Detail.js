0c8f02b2547fa0b35322687a3915998ba25056d7
hanbinchoi <wkghskak@naver.com>
2023-04-04 16:10:43 +0900

    230404

---
+++ my-app/src/routes/Detail.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+function Detail() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState([]);
+    const getMovie = async () => {
+        const json = await (
+            await fetch(
+                `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+            )
+        ).json();
+        console.log(json.data.movie);
+        setMovie(json.data.movie);
+    };
+    useEffect(() => {
+        getMovie();
+    }, []);
+    return (
+        <div>
+            <img src={movie.medium_cover_image} alt="" />
+            <h1>{movie.title}</h1>
+            <p>{movie.description_full}</p>
+            <ul>
+                {movie.genres.map((g) => (
+                    <li key={g}>{g}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+export default Detail;
